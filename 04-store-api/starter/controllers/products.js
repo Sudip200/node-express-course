b7dbf948b9e1e14faa428f8123ab1459e9a935f9
Sudipto Das <dassudipto200@gmail.com>
2022-10-20 06:39:07 +0000

    adding data to to db and adding basic controller

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,8 +1,15 @@
+
+const Products=require('../models/product')
+
 const getAllProductsStatic= async (req,res)=>{
-res.status(200).json({msg:'Product testing route'})
+const products=await Products.find({})
+res.status(200).json({products,nbHits:products.length})
 }
 const getAllProducts=async (req,res)=>{
-res.status(200).json({msg:'product route'})
+console.log(req.query)
+const val=req.query;
+const products=await Products.find(val);
+res.status(200).json({products,nbHits:products.length})
 }
 module.exports={
     getAllProducts,
